5080dd4464315cbd8496d805f7d8a82f90329424
Desoju <desovajulie@seznam.cz>
2024-03-01 15:27:19 +0100

    opacity

---
+++ src/pages/SeznamPrihlasenych/PrihlseniUcastnici.jsx
@@ -0,0 +1,7 @@
+export const PrihlaseniUcastnci = [
+  { name: "Dukáš", surname: "Les", age: 16 },
+  { name: "Pukáš", surname: "Ves", age: 15 },
+  { name: "Hukáš", surname: "Mes", age: 14 },
+  { name: "Lukáš", surname: "Pes", age: 13 },
+  { name: "Makáš", surname: "Des", age: 13 },
+];
--- src/pages/SeznamPrihlasenych/index.jsx
+++ src/pages/SeznamPrihlasenych/index.jsx
@@ -1,9 +1,32 @@
 import "./style.scss";
+import { PrihlaseniUcastnci } from "./PrihlseniUcastnici";
 
 export const SeznamPrihlasenych = () => {
   return (
     <div className="seznam_prihlasenych">
       <h1>Seznam přihlášených</h1>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Jméno</th>
+            <th>Přijmení</th>
+            <th>Věk</th>
+          </tr>
+        </thead>
+        <tbody>
+          {PrihlaseniUcastnci.map((val, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{val.name}</td>
+                <td>{val.surname}</td>
+                <td>{val.age}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
