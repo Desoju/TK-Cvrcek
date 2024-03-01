8577e5d52ba8aa29447c6cf2dad4b500df973285
Desoju <desovajulie@seznam.cz>
2024-03-01 15:28:07 +0100

    seznam ucastniku

--- src/pages/SeznamPrihlasenych/PrihlseniUcastnici.jsx
+++ src/pages/SeznamPrihlasenych/PrihlseniUcastnici.jsx
@@ -1,7 +1,3 @@
 export const PrihlaseniUcastnci = [
-  { name: "Dukáš", surname: "Les", age: 16 },
-  { name: "Pukáš", surname: "Ves", age: 15 },
-  { name: "Hukáš", surname: "Mes", age: 14 },
-  { name: "Lukáš", surname: "Pes", age: 13 },
-  { name: "Makáš", surname: "Des", age: 13 },
+  { name: "Jan", surname: "Kratochvíl", age: 10 },
 ];
