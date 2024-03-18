572fdc2e7f4c6d49bea90e2cc46c962184ff1c13
GhostDraagon <gkninb@gmail.com>
2024-03-18 20:15:38 +0100

    přihl účast

--- src/pages/SeznamPrihlasenych/index.jsx
+++ src/pages/SeznamPrihlasenych/index.jsx
@@ -5,7 +5,7 @@ export const SeznamPrihlasenych = () => {
   return (
     <div className="seznam_prihlasenych">
       <h1>Seznam přihlášených</h1>
-      <p>Jarní obůrka 5.-7. 4. 2024   |    Předtáborová Obůrka 21.-23. 6. 2024</p>
+      <p>Jarní obůrka 5.-7. 4. 2024</p>
       <table>
         <thead>
           <tr>
@@ -21,7 +21,6 @@ export const SeznamPrihlasenych = () => {
                 <td>{index + 1}</td>
                 <td>{val.name}</td>
                 <td>{val.paid}</td>
-                <td>    |    </td>
               </tr>
             );
           })}
