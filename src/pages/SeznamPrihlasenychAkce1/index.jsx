ac70088cd46e3473d653b0b42272b3b10d55dc20
GhostDraagon <gkninb@gmail.com>
2024-04-02 22:13:55 +0200

    jmena

--- src/pages/SeznamPrihlasenychAkce1/index.jsx
+++ src/pages/SeznamPrihlasenychAkce1/index.jsx
@@ -6,7 +6,7 @@ export const SeznamPrihlasenychAkce1 = () => {
     <div className="seznam_prihlasenych">
       <h1>Seznam přihlášených</h1>
       <p>Jarní obůrka 5.-7. 4. 2024</p>
-      <p>Aktualizováno ke dni 1. 4. 2024</p>
+      <p>Aktualizováno ke dni 2. 4. 2024</p>
       <table>
         <thead>
           <tr>
