a8db102409669d35a991b8f660f149b0dd688d2d
GhostDraagon <gkninb@gmail.com>
2024-04-04 18:59:07 +0200

    jmena

--- src/pages/SeznamPrihlasenychTabor/index.jsx
+++ src/pages/SeznamPrihlasenychTabor/index.jsx
@@ -5,7 +5,7 @@ export const SeznamPrihlasenychTabor = () => {
     <div className="seznam_prihlasenych">
       <h1>Seznam přihlášených</h1>
       <p>Letní tábor 14.-27. 7. 2024</p>
-      <p>Aktualizováno ke dni 2. 4. 2024</p>
+      <p>Aktualizováno ke dni 4. 4. 2024</p>
       <table>
         <thead>
           <tr>
