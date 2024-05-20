05bd8dbc8e574ecc26486e77b78c1dff661cc10f
GhostDraagon <gkninb@gmail.com>
2024-05-20 21:59:27 +0200

    oprava

--- src/pages/SeznamPrihlasenychAkce1/index.jsx
+++ src/pages/SeznamPrihlasenychAkce1/index.jsx
@@ -5,7 +5,7 @@ export const SeznamPrihlasenychAkce1 = () => {
   return (
     <div className="seznam_prihlasenych">
       <h1>Seznam přihlášených</h1>
-      <p>Jarní obůrka 21.-23. 6. 2024</p>
+      <p>Letní obůrka 21.-23. 6. 2024</p>
       <p>Aktualizováno ke dni 20. 5. 2024</p>
       <table>
         <thead>
