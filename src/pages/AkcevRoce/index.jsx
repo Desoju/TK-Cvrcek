8d24f21229aacbbe5679feb2ef57c69267d07410
GhostDraagon <gkninb@gmail.com>
2024-10-01 22:03:31 +0200

    ss

--- src/pages/AkcevRoce/index.jsx
+++ src/pages/AkcevRoce/index.jsx
@@ -19,6 +19,8 @@ export const AkcevRoce = () => {
           <p>27. 10. 2024</p>
           <a href={podzim}>INFORMACE</a>
           <br></br>
+          <a href={"https://docs.google.com/spreadsheets/d/1-Z_ZxacciCQ2-M4izrraOPYBLY6zublwkhwF3buQHbk/edit?fbclid=IwZXh0bgNhZW0CMTEAAR1H5-eWpdy72G5KGEQdtKXm3dUuVtF9u7gV5j_TrLJLtkqT-k1yqH_x7VA_aem_taerbto3iNLO-oKg9Z9Iyw&gid=0#gid=0"}>TABULKA NA TÝMY </a>
+          <br></br>
           <Link to="/seznamprihlasenychAkce1" className="link">
             SEZNAM PŘIHLÁŠENÝCH
           </Link>
