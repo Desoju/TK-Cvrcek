4cb7c821775a5132f52d7ce255fe84c8fa7f090d
GhostDraagon <gkninb@gmail.com>
2024-10-08 11:32:54 +0200

    drakiada

--- src/pages/AkcevRoce/index.jsx
+++ src/pages/AkcevRoce/index.jsx
@@ -1,6 +1,7 @@
 import "./style.scss";
 import { Link } from "react-router-dom";
 import podzim from "./../../documents/podzim-sifryy.pdf"
+import drak from "./../../documents/Drakiáda-info.pdf"
 
 export const AkcevRoce = () => {
   return (
@@ -20,38 +21,30 @@ export const AkcevRoce = () => {
           <a href={podzim}>INFORMACE</a>
           <br></br>
           <a href={"https://docs.google.com/spreadsheets/d/1-Z_ZxacciCQ2-M4izrraOPYBLY6zublwkhwF3buQHbk/edit?fbclid=IwZXh0bgNhZW0CMTEAAR1H5-eWpdy72G5KGEQdtKXm3dUuVtF9u7gV5j_TrLJLtkqT-k1yqH_x7VA_aem_taerbto3iNLO-oKg9Z9Iyw&gid=0#gid=0"}>TABULKA NA TÝMY </a>
-          <br></br>
+          {/*<br></br>
           <Link to="/seznamprihlasenychAkce1" className="link">
             SEZNAM PŘIHLÁŠENÝCH
-          </Link>
+          </Link> */}
           
         </div>
         <div className="akce-v-roce__momentalini_akce__akce2">
-          <h2>Drakiáda</h2>
+          <h2>Drakiáda Lukovany</h2>
           <p>9.-10. 11. 2024</p>
-          <p></p>
-          {/* <a href="" className="prozatim">INFO (zatím není)</a> */}
-          {/*<p>Přihlášení účastníci</p>*/}
+          <a href={drak}>INFORMACE</a>
+          <br></br>
+          <Link to="/seznamprihlasenychAkce2" className="link">
+            SEZNAM PŘIHLÁŠENÝCH
+          </Link>
         </div>
         {/*<div className="akce-v-roce__momentalini_akce__akce3">
-          <p>Název</p>
-          <p>Datum</p>
+          <p>Drakiáda</p>
+          <p>datum</p>
           <p>Info</p>
-          <p>Přihlášení účastníci</p>
-        </div> */}
-        <div className="akce-v-roce__momentalini_akce__akce3">
-          <h2></h2>
-          <p></p>
-          <p></p>
-          {/* <a href="" className="prozatim">INFO (zatím není)</a> */}
-          {/*<p>Přihlášení účastníci</p>*/}
-        </div>
-        {/*<div className="akce-v-roce__momentalini_akce__akce3">
-          <p>Název</p>
-          <p>Datum</p>
-          <p>Info</p>
-          <p>Přihlášení účastníci</p>
-        </div> */}
+          <Link to="/seznamprihlasenychAkce2" className="link">
+            SEZNAM PŘIHLÁŠENÝCH
+          </Link>
+        </div> 
+        */}
       </div>
     </div>
   );
