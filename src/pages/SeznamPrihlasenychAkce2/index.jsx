aa644f31afa71c3fe0cc63a69a277057c966e86f
GhostDraagon <gkninb@gmail.com>
2025-05-16 20:03:54 +0200

    update

--- src/pages/SeznamPrihlasenychAkce2/index.jsx
+++ src/pages/SeznamPrihlasenychAkce2/index.jsx
@@ -5,7 +5,7 @@ export const SeznamPrihlasenychAkce2 = () => {
     <div className="seznam_prihlasenych">
       <h1>Seznam přihlášených</h1>
       <p>Vlaková loupež Meziboří</p>
-      <p>Aktualizováno ke dni 12. 5. 2025</p>
+      <p>Aktualizováno ke dni 16. 5. 2025</p>
       <table>
         <thead>
           <tr>
